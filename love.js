79e366242ec5e3a94563193bdb7e3675d171fe3a
Ashutosh Mishra <131657477+ashutoshmishra52@users.noreply.github.com>
2024-05-05 12:56:02 +0530

    Add files via upload

---
+++ love.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  // Function to simulate interactions with the page
+  function simulateInteraction() {
+    // Customize your interactions here
+    // For example, you can trigger a click on an element
+    $(".ico").click();
+  }
+
+  // Set an interval to simulate interactions every 1000 milliseconds (1 second)
+  setInterval(simulateInteraction, 1000);
+});
